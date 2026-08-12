05b266fda16cd4aa0612d0eea456c05a1f03b02d
Vitorr-AF <vitoraugustofranca7@gmail.com>
2026-08-11 22:36:41 -0300

    Controle com joystick adicionado

--- controle.py
+++ controle.py
@@ -1,14 +1,42 @@
 import serial
 import time
+import pygame
 
 # Colocar porta do ESP
 esp = serial.Serial("COM3", 115200)
 
 time.sleep(2)
 
+velocidade = 20
 
-modificador = 10
+# Código pra mandar comandos pro ESP (modificador é em graus)
+def mover_vertical(modificador):
+    esp.write(f"VERTICAL:{modificador}\n".encode())
 
-# Código pra mandar comandos pro ESP
-esp.write(f"VERTICAL:{modificador}\n".encode())
-esp.write(f"HORIZONTAL:{modificador}\n".encode())
+def mover_horizontal(modificador):
+    esp.write(f"HORIZONTAL:{modificador}\n".encode())
+
+
+
+pygame.init()
+pygame.joystick.init()
+
+
+
+while True:
+    pygame.event.pump()
+    if pygame.joystick.get_count() > 0:
+
+        if controle is None:
+            controle = pygame.joystick.Joystick(0)
+            controle.init()
+        eixo_x = controle.get_axis(0)
+        eixo_y = controle.get_axis(1)
+
+        if eixo_x > 0.5 or eixo_x < -0.5:
+            mover_horizontal(int(velocidade * eixo_x))
+        if eixo_y > 0.5 or eixo_y < -0.5:
+            mover_vertical(int(velocidade * eixo_y))
+    else:
+        controle = None
+    time.sleep(0.05)
